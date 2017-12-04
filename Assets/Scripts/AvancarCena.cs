613c2bd94e7d06e6b66b166c31dcbd7afae61e82
logonrm <logonrm@fiap.com.br>
2017-12-04 20:25:54 -0200

    first commit

---
+++ Assets/Scripts/AvancarCena.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AvancarCena : MonoBehaviour {
+
+	public string cena;
+
+	
+	// Update is called once per frame
+	void Update () {
+		//Fire1 --> touch, ctrl+left, click+left
+		if (Input.GetButtonDown ("Fire1")) {
+			SceneManager.LoadScene (cena);
+		}	
+	}
+}
